f4fbfc5751dc05a93ba364f50d548130e0ad4c39
zhanghaojie <1455123087@qq.com>
2024-02-06 15:21:52 +0800

    feat: layout animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import { FadeInAndOut } from './FadeInAndOut';
+import { LayoutGroup, motion } from 'framer-motion';
+import React, { useState } from 'react';
+
+let id = 0;
+
+function createItem() {
+  return {
+    id: id++,
+    text: `new item ${id}`,
+  };
+}
 
 function App() {
+  const [list, setList] = useState(() => [createItem()]);
+  function removeItem(id: number) {
+    setList(list.filter((item) => item.id !== id));
+  }
   return (
     <>
-      <div>
-        <FadeInAndOut />
-      </div>
+      <LayoutGroup>
+        <button
+          type="button"
+          className="rounded-md text-sm px-3 py-1 bg-blue-400 hover:bg-blue-500 text-white align-middle"
+          onClick={() => setList((prev) => [...prev, createItem()])}
+        >
+          new item
+        </button>
+        <div className="flex flex-col gap-2">
+          {list.map((item) => (
+            <motion.div
+              key={item.id}
+              layout="position"
+              className="flex justify-between py-4 px-3 items-center bg-gray-200 rounded-md shadow-md w-1/4 hover:shadow-lg ease-in-out"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              {item.text}
+              <button
+                type="button"
+                onClick={() => removeItem(item.id)}
+                className="rounded-md text-sm px-3 py-1 bg-red-400 hover:bg-red-500 text-white align-middle"
+              >
+                x
+              </button>
+            </motion.div>
+          ))}
+        </div>
+      </LayoutGroup>
     </>
   );
 }
