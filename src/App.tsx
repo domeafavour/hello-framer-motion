4d7d95c2333e0626bb21d16ec2dd5d3b12bd5166
zhanghaojie <1455123087@qq.com>
2024-02-06 14:38:54 +0800

    fade in & fade out

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <>
-      <h1>Hello World!</h1>
+      <div>
+        <button type="button" onClick={() => setShow(!show)}>
+          toggle show
+        </button>
+        <AnimatePresence>
+          {show && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              hello world
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </>
   );
 }
